d9177419f4fbc002e6cf22fe50cc956dbfd3cd23
AlbertM96 <57848310+AlbertM96@users.noreply.github.com>
2022-05-23 20:51:25 +0200

    ModifMedecinOKmais pas optimal
    faut saisir l'id manuellement, c pour ça q c pas optimal, mais fait le boulot

--- fonctionnalitesAdminModifMedecin.php
+++ fonctionnalitesAdminModifMedecin.php
@@ -2,7 +2,7 @@
 include "connexionBDD.php";
 echo "page modification medecin <br>";
 if($BDDTrouvee){
-	echo "BDD existe";
+	echo "BDD existe<br><br>";
 }
 ?>
 
@@ -11,12 +11,13 @@ if($BDDTrouvee){
 <head>
 </head>
 <body>
-		<form class="formulaireModifMedecin">
+		<form method = "POST" class="formulaireModifMedecin">
 			<br />VEUILLEZ CHOISIR LE MEDECIN A MODIFIER: <select name="medecinAModifier">
 				<option>choisir...</option>
 				<option value=1>Nom1 Prénom1</option>
 				<option value=2>Nom2 Prénom2</option>
 			</select>
+			<br>Option alternative: saisir l'id du médecin à modifier: <input type="text" name="idModifTextField">
 			<br /><br />VEUILLEZ SAISIR LES INFORMATIONS A MODIFIER<br />
 			Nom: <input type="text" name="nomMedecin" /> <br />
 			Prenom: <input type="text" name="prenomMedecin" /> <br />
@@ -37,7 +38,26 @@ if($BDDTrouvee){
 			Téléphone: <input type="number" name="telMedecin" /> <br />
 			Formation CV (50 caractères maximum): <br /> <textarea maxlength="50" name="formationsCVMedecin"></textarea> <br />
 			Expériences CV (50 caractères maximum): <br /> <textarea maxlength="50" name="experiencesCVMedecin"></textarea> <br />
-			<input type="submit" name="ordreModification" value="Modifier ce médecin" />
+			<input type="submit" name="ordreModification" value="Modifier ce médecin" /> <br><br>
+		
+			<?php 	
+			if(isset($_POST['ordreModification'])){
+				//$idAdminActuel = 1;
+				$nom = isset($_POST['nomMedecin']) ? $_POST['nomMedecin'] : " ";
+				$prenom = isset($_POST['prenomMedecin']) ? $_POST['prenomMedecin'] : " ";
+				$specialite = isset($_POST['specialite']) ? $_POST['specialite'] :" " ;
+				$bureau = isset($_POST['bureauMedecin']) ? $_POST['bureauMedecin'] : " ";
+				$tel = isset($_POST['telMedecin']) ? $_POST['telMedecin'] : 0;
+				$courriel = isset($_POST['courrielMedecin']) ? $_POST['courrielMedecin'] : " ";
+				$formations = isset($_POST['formationsCVMedecin']) ? $_POST['formationsCVMedecin'] :" ";
+				$experiences = isset($_POST['experiencesCVMedecin']) ? $_POST['experiencesCVMedecin'] : " ";
+				$idAModifier = isset($_POST['idModifTextField']) ? $_POST['idModifTextField'] : 0;
+				$requete = "UPDATE `hopital`.`medecin` SET `nomMedecin` = '$nom', `prenomMedecin` = '$prenom', `specialiteMedecin` = '$specialite', `bureauMedecin` = '$bureau', `telMedecin` = '$tel', `courrielMedecin` = '$courriel', `formationCV` = '$formations', `experiencesCV` = '$experiences' WHERE `medecin`.`idMedecin` = $idAModifier;";
+				echo "requete formulée $requete";
+				$commande = mysqli_query($loginBDD, $requete);
+				echo "<br>Modification médecin avec id $idAModifier OK<br><br>";
+			}
+			?>		
 		</form>
 </body>
 </html>
