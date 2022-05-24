ec3683430597b362a420b6680bec11dc2347bcbd
AlbertM96 <57848310+AlbertM96@users.noreply.github.com>
2022-05-24 19:20:36 +0200

    ConnexionAdmin&Médecin OK

--- loginAdmin.php
+++ loginAdmin.php
@@ -17,13 +17,22 @@ if($BDDTrouvee){
 		Prénom: <input type="text" name="prenomAdmin" /> <br />
 		Courriel: <input type="email" name="courrielAdmin" /> <br />
 		<input type="submit" name="loginEspaceAdmin" value="Se connecter" />
-	</form>
 
 <?php
-$nom = isset($_POST['nomAdmin']) ? isset($_POST['nomAdmin']) : " "; 
-$prenom = isset($_POST['prenomAdmin']) ? isset($_POST['prenomAdmin']) : " "; 
-$courriel = isset($_POST['courrielAdmin']) ? isset($_POST['courrielAdmin']) : " "; 
+if(isset($_POST['loginEspaceAdmin'])){
+	$nom = isset($_POST['nomAdmin']) ? $_POST['nomAdmin'] : " "; 
+	$prenom = isset($_POST['prenomAdmin']) ? $_POST['prenomAdmin'] : " "; 
+	$courriel = isset($_POST['courrielAdmin']) ? $_POST['courrielAdmin'] : " ";
+	
+	$requete = "SELECT * FROM `admin` where (nomadmin = '$nom' and prenomadmin = '$prenom' and courrieladmin = '$courriel');";
+	$commande = mysqli_query($loginBDD, $requete);
+	$resultat = "on sait pas encore"; $donnee = mysqli_fetch_assoc($commande);
+	if($donnee['idAdmin']!=null){$resultat="Connexion réussie";} else{$resultat="Aucun client trouvé";}
+	echo "<br>requete formulée $requete";
+	echo "<br>Resultat identification: $resultat<br><br>";
+}
 ?>
 
+	</form>
 </body>
 </html>
