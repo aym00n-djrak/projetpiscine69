62d714d1ab131e4c5e5499cc88a6b766e0bb219f
AlbertM96 <57848310+AlbertM96@users.noreply.github.com>
2022-05-24 16:01:12 +0200

    Pb BDD résolu
    On peut désormais:
    
    -Associer un service à un ou plusieurs labo
    -Associer un paiement à un service
    -Associer un message à 1 médecin et 1 client (obligatoire)
    
    PS: Ré-importez le fichier sql dans votre bdd

--- hopital.sql
+++ hopital.sql
@@ -1,269 +1,165 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Client :  127.0.0.1
--- Généré le :  Dim 22 Mai 2022 à 23:52
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+#------------------------------------------------------------
+# Table: admin
+#------------------------------------------------------------
 
---
--- Base de données :  `hopital`
---
+CREATE TABLE admin(
+        idAdmin       Int  Auto_increment  NOT NULL ,
+        nomAdmin      Varchar (50) NOT NULL ,
+        prenomAdmin   Varchar (50) NOT NULL ,
+        courrielAdmin Varchar (50) NOT NULL
+	,CONSTRAINT admin_PK PRIMARY KEY (idAdmin)
+)ENGINE=InnoDB;
 
--- --------------------------------------------------------
 
---
--- Structure de la table `admin`
---
+#------------------------------------------------------------
+# Table: labo
+#------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `idAdmin` int(11) NOT NULL AUTO_INCREMENT,
-  `nomAdmin` varchar(50) NOT NULL,
-  `prenomAdmin` varchar(50) NOT NULL,
-  `courrielAdmin` varchar(50) NOT NULL,
-  PRIMARY KEY (`idAdmin`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+CREATE TABLE labo(
+        idLabo       Int  Auto_increment  NOT NULL ,
+        salleLabo    Varchar (50) NOT NULL ,
+        telLabo      Int NOT NULL ,
+        courrielLabo Varchar (50) NOT NULL ,
+        idAdmin      Int NOT NULL
+	,CONSTRAINT labo_PK PRIMARY KEY (idLabo)
 
---
--- Contenu de la table `admin`
---
+	,CONSTRAINT labo_admin_FK FOREIGN KEY (idAdmin) REFERENCES admin(idAdmin)
+)ENGINE=InnoDB;
 
-INSERT INTO `admin` (`idAdmin`, `nomAdmin`, `prenomAdmin`, `courrielAdmin`) VALUES
-(1, 'LeThug', 'Alberto', 'alberto.lethug@omnesHopital.fr');
 
--- --------------------------------------------------------
+#------------------------------------------------------------
+# Table: medecin
+#------------------------------------------------------------
 
---
--- Structure de la table `client`
---
+CREATE TABLE medecin(
+        idMedecin         Int  Auto_increment  NOT NULL ,
+        nomMedecin        Varchar (50) NOT NULL ,
+        prenomMedecin     Varchar (50) NOT NULL ,
+        specialiteMedecin Varchar (50) NOT NULL ,
+        bureauMedecin     Varchar (50) NOT NULL ,
+        telMedecin        Int NOT NULL ,
+        courrielMedecin   Varchar (50) NOT NULL ,
+        formationCV       Varchar (50) NOT NULL ,
+        experiencesCV     Varchar (50) NOT NULL ,
+        idAdmin           Int NOT NULL
+	,CONSTRAINT medecin_PK PRIMARY KEY (idMedecin)
 
-CREATE TABLE IF NOT EXISTS `client` (
-  `idClient` int(11) NOT NULL AUTO_INCREMENT,
-  `nomClient` varchar(50) NOT NULL,
-  `prenomClient` varchar(50) NOT NULL,
-  `courrielClient` varchar(50) NOT NULL,
-  `motDePasseClient` varchar(50) NOT NULL,
-  `adresseClient` varchar(50) NOT NULL,
-  `villeClient` varchar(50) NOT NULL,
-  `codePostalClient` int(11) NOT NULL,
-  `paysClient` varchar(50) NOT NULL,
-  `telClient` int(11) NOT NULL,
-  `carteVitaleClient` int(11) NOT NULL,
-  `idAdmin` int(11) NOT NULL,
-  PRIMARY KEY (`idClient`),
-  KEY `Client_admin_FK` (`idAdmin`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+	,CONSTRAINT medecin_admin_FK FOREIGN KEY (idAdmin) REFERENCES admin(idAdmin)
+)ENGINE=InnoDB;
 
---
--- Contenu de la table `client`
---
 
-INSERT INTO `client` (`idClient`, `nomClient`, `prenomClient`, `courrielClient`, `motDePasseClient`, `adresseClient`, `villeClient`, `codePostalClient`, `paysClient`, `telClient`, `carteVitaleClient`, `idAdmin`) VALUES
-(1, 'Gibraltar', 'Friedrich', 'friedrich.gibraltar@hotmail.com', 'leGnangnan', '890, Park Avenue', 'NY', 6921, 'USA', 904884, 540044, 1);
+#------------------------------------------------------------
+# Table: Client
+#------------------------------------------------------------
 
--- --------------------------------------------------------
+CREATE TABLE Client(
+        idClient          Int  Auto_increment  NOT NULL ,
+        nomClient         Varchar (50) NOT NULL ,
+        prenomClient      Varchar (50) NOT NULL ,
+        courrielClient    Varchar (50) NOT NULL ,
+        motDePasseClient  Varchar (50) NOT NULL ,
+        adresseClient     Varchar (50) NOT NULL ,
+        villeClient       Varchar (50) NOT NULL ,
+        codePostalClient  Int NOT NULL ,
+        paysClient        Varchar (50) NOT NULL ,
+        telClient         Int NOT NULL ,
+        carteVitaleClient Int NOT NULL ,
+        idAdmin           Int NOT NULL
+	,CONSTRAINT Client_PK PRIMARY KEY (idClient)
 
---
--- Structure de la table `comptebancaire`
---
+	,CONSTRAINT Client_admin_FK FOREIGN KEY (idAdmin) REFERENCES admin(idAdmin)
+)ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `comptebancaire` (
-  `idCompte` int(11) NOT NULL AUTO_INCREMENT,
-  `typeCarteCompte` varchar(50) NOT NULL,
-  `numCarteCompte` int(11) NOT NULL,
-  `nomCarteCompte` varchar(50) NOT NULL,
-  `dateExpirationCarteCompte` date NOT NULL,
-  `codeSecuriteCarteCompte` int(11) NOT NULL,
-  `idClient` int(11) NOT NULL,
-  PRIMARY KEY (`idCompte`),
-  KEY `compteBancaire_Client_FK` (`idClient`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
---
--- Contenu de la table `comptebancaire`
---
+#------------------------------------------------------------
+# Table: creneau
+#------------------------------------------------------------
 
-INSERT INTO `comptebancaire` (`idCompte`, `typeCarteCompte`, `numCarteCompte`, `nomCarteCompte`, `dateExpirationCarteCompte`, `codeSecuriteCarteCompte`, `idClient`) VALUES
-(1, 'MasterCard', 55408, 'FRIEDRICH GIBRALTAR', '2022-08-31', 58484, 1);
+CREATE TABLE creneau(
+        idCreneau         Int  Auto_increment  NOT NULL ,
+        dateCreneau       Date NOT NULL ,
+        heureCreneau      Time NOT NULL ,
+        reserveOuPas      Bool NOT NULL ,
+        consultationFinie Bool NOT NULL ,
+        idClient          Int ,
+        idMedecin         Int ,
+        idLabo            Int
+	,CONSTRAINT creneau_PK PRIMARY KEY (idCreneau)
 
--- --------------------------------------------------------
+	,CONSTRAINT creneau_Client_FK FOREIGN KEY (idClient) REFERENCES Client(idClient)
+	,CONSTRAINT creneau_medecin0_FK FOREIGN KEY (idMedecin) REFERENCES medecin(idMedecin)
+	,CONSTRAINT creneau_labo1_FK FOREIGN KEY (idLabo) REFERENCES labo(idLabo)
+)ENGINE=InnoDB;
 
---
--- Structure de la table `creneau`
---
 
-CREATE TABLE IF NOT EXISTS `creneau` (
-  `idCreneau` int(11) NOT NULL AUTO_INCREMENT,
-  `dateCreneau` date NOT NULL,
-  `heureCreneau` time NOT NULL,
-  `reserveOuPas` tinyint(1) NOT NULL,
-  `consultationFinie` tinyint(1) NOT NULL,
-  `idClient` int(11) DEFAULT NULL,
-  `idMedecin` int(11) DEFAULT NULL,
-  `idLabo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idCreneau`),
-  KEY `creneau_Client_FK` (`idClient`),
-  KEY `creneau_medecin0_FK` (`idMedecin`),
-  KEY `creneau_labo1_FK` (`idLabo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+#------------------------------------------------------------
+# Table: compteBancaire
+#------------------------------------------------------------
 
---
--- Contenu de la table `creneau`
---
+CREATE TABLE compteBancaire(
+        idCompte                  Int  Auto_increment  NOT NULL ,
+        typeCarteCompte           Varchar (50) NOT NULL ,
+        numCarteCompte            Int NOT NULL ,
+        nomCarteCompte            Varchar (50) NOT NULL ,
+        dateExpirationCarteCompte Date NOT NULL ,
+        codeSecuriteCarteCompte   Int NOT NULL ,
+        idClient                  Int NOT NULL
+	,CONSTRAINT compteBancaire_PK PRIMARY KEY (idCompte)
 
-INSERT INTO `creneau` (`idCreneau`, `dateCreneau`, `heureCreneau`, `reserveOuPas`, `consultationFinie`, `idClient`, `idMedecin`, `idLabo`) VALUES
-(1, '2022-05-31', '14:55:00', 0, 0, NULL, 1, NULL),
-(2, '2022-06-14', '14:55:00', 0, 0, NULL, NULL, 1),
-(3, '2022-07-20', '14:55:00', 1, 0, 1, NULL, 2);
+	,CONSTRAINT compteBancaire_Client_FK FOREIGN KEY (idClient) REFERENCES Client(idClient)
+)ENGINE=InnoDB;
 
--- --------------------------------------------------------
 
---
--- Structure de la table `labo`
---
+#------------------------------------------------------------
+# Table: serviceLabo
+#------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `labo` (
-  `idLabo` int(11) NOT NULL AUTO_INCREMENT,
-  `salleLabo` varchar(50) NOT NULL,
-  `telLabo` int(11) NOT NULL,
-  `courrielLabo` varchar(50) NOT NULL,
-  `idAdmin` int(11) NOT NULL,
-  PRIMARY KEY (`idLabo`),
-  KEY `labo_admin_FK` (`idAdmin`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+CREATE TABLE serviceLabo(
+        idServiceLabo    Int  Auto_increment  NOT NULL ,
+        nomServiceLabo   Varchar (50) NOT NULL ,
+        tarifServiceLabo Float NOT NULL ,
+        idLabo           Int NOT NULL
+	,CONSTRAINT serviceLabo_PK PRIMARY KEY (idServiceLabo)
 
---
--- Contenu de la table `labo`
---
+	,CONSTRAINT serviceLabo_labo_FK FOREIGN KEY (idLabo) REFERENCES labo(idLabo)
+)ENGINE=InnoDB;
 
-INSERT INTO `labo` (`idLabo`, `salleLabo`, `telLabo`, `courrielLabo`, `idAdmin`) VALUES
-(1, 'E430', 0, 'laboE430@omnesHopital.fr', 1),
-(2, 'E430', 5642555, 'laboE430@omnesHopital.fr', 1);
 
--- --------------------------------------------------------
+#------------------------------------------------------------
+# Table: message
+#------------------------------------------------------------
 
---
--- Structure de la table `medecin`
---
+CREATE TABLE message(
+        idMessage      Int  Auto_increment  NOT NULL ,
+        contenuMessage Varchar (150) NOT NULL ,
+        dateMessage    Date NOT NULL ,
+        heureMessage   Time NOT NULL ,
+        idMedecin      Int NOT NULL ,
+        idClient       Int NOT NULL
+	,CONSTRAINT message_PK PRIMARY KEY (idMessage)
 
-CREATE TABLE IF NOT EXISTS `medecin` (
-  `idMedecin` int(11) NOT NULL AUTO_INCREMENT,
-  `nomMedecin` varchar(50) NOT NULL,
-  `prenomMedecin` varchar(50) NOT NULL,
-  `specialiteMedecin` varchar(50) NOT NULL,
-  `bureauMedecin` varchar(50) NOT NULL,
-  `telMedecin` int(11) NOT NULL,
-  `courrielMedecin` varchar(50) NOT NULL,
-  `formationCV` varchar(50) NOT NULL,
-  `experiencesCV` varchar(50) NOT NULL,
-  `idAdmin` int(11) NOT NULL,
-  PRIMARY KEY (`idMedecin`),
-  KEY `medecin_admin_FK` (`idAdmin`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+	,CONSTRAINT message_medecin_FK FOREIGN KEY (idMedecin) REFERENCES medecin(idMedecin)
+	,CONSTRAINT message_Client0_FK FOREIGN KEY (idClient) REFERENCES Client(idClient)
+)ENGINE=InnoDB;
 
---
--- Contenu de la table `medecin`
---
 
-INSERT INTO `medecin` (`idMedecin`, `nomMedecin`, `prenomMedecin`, `specialiteMedecin`, `bureauMedecin`, `telMedecin`, `courrielMedecin`, `formationCV`, `experiencesCV`, `idAdmin`) VALUES
-(1, 'AimeNichons', 'Vladimir', 'Gynecologie', 'E455', 5440968, 'vlad.aimenichons@omnesHopital.com', '1999-2009: UPMC médecine générale; 1990-1999:t', '2010-2020: Versailles gyné de Marie Antoinette', 1);
+#------------------------------------------------------------
+# Table: paiement
+#------------------------------------------------------------
 
--- --------------------------------------------------------
+CREATE TABLE paiement(
+        idLabo        Int NOT NULL ,
+        idCompte      Int NOT NULL ,
+        idServiceLabo Int NOT NULL
+	,CONSTRAINT paiement_PK PRIMARY KEY (idLabo,idCompte,idServiceLabo)
 
---
--- Structure de la table `paiement`
---
+	,CONSTRAINT paiement_labo_FK FOREIGN KEY (idLabo) REFERENCES labo(idLabo)
+	,CONSTRAINT paiement_compteBancaire0_FK FOREIGN KEY (idCompte) REFERENCES compteBancaire(idCompte)
+	,CONSTRAINT paiement_serviceLabo1_FK FOREIGN KEY (idServiceLabo) REFERENCES serviceLabo(idServiceLabo)
+)ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `paiement` (
-  `idLabo` int(11) NOT NULL,
-  `idServiceLabo` int(11) NOT NULL,
-  `idCompte` int(11) NOT NULL,
-  PRIMARY KEY (`idLabo`,`idServiceLabo`,`idCompte`),
-  KEY `paiement_serviceLabo0_FK` (`idServiceLabo`),
-  KEY `paiement_compteBancaire1_FK` (`idCompte`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `paiement`
---
-
-INSERT INTO `paiement` (`idLabo`, `idServiceLabo`, `idCompte`) VALUES
-(2, 1, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `servicelabo`
---
-
-CREATE TABLE IF NOT EXISTS `servicelabo` (
-  `idServiceLabo` int(11) NOT NULL AUTO_INCREMENT,
-  `nomServiceLabo` varchar(50) NOT NULL,
-  `tarifServiceLabo` float NOT NULL,
-  PRIMARY KEY (`idServiceLabo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Contenu de la table `servicelabo`
---
-
-INSERT INTO `servicelabo` (`idServiceLabo`, `nomServiceLabo`, `tarifServiceLabo`) VALUES
-(1, 'Biologie préventive', 455.3);
-
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `client`
---
-ALTER TABLE `client`
-  ADD CONSTRAINT `Client_admin_FK` FOREIGN KEY (`idAdmin`) REFERENCES `admin` (`idAdmin`);
-
---
--- Contraintes pour la table `comptebancaire`
---
-ALTER TABLE `comptebancaire`
-  ADD CONSTRAINT `compteBancaire_Client_FK` FOREIGN KEY (`idClient`) REFERENCES `client` (`idClient`);
-
---
--- Contraintes pour la table `creneau`
---
-ALTER TABLE `creneau`
-  ADD CONSTRAINT `creneau_Client_FK` FOREIGN KEY (`idClient`) REFERENCES `client` (`idClient`),
-  ADD CONSTRAINT `creneau_medecin0_FK` FOREIGN KEY (`idMedecin`) REFERENCES `medecin` (`idMedecin`),
-  ADD CONSTRAINT `creneau_labo1_FK` FOREIGN KEY (`idLabo`) REFERENCES `labo` (`idLabo`);
-
---
--- Contraintes pour la table `labo`
---
-ALTER TABLE `labo`
-  ADD CONSTRAINT `labo_admin_FK` FOREIGN KEY (`idAdmin`) REFERENCES `admin` (`idAdmin`);
-
---
--- Contraintes pour la table `medecin`
---
-ALTER TABLE `medecin`
-  ADD CONSTRAINT `medecin_admin_FK` FOREIGN KEY (`idAdmin`) REFERENCES `admin` (`idAdmin`);
-
---
--- Contraintes pour la table `paiement`
---
-ALTER TABLE `paiement`
-  ADD CONSTRAINT `paiement_labo_FK` FOREIGN KEY (`idLabo`) REFERENCES `labo` (`idLabo`),
-  ADD CONSTRAINT `paiement_serviceLabo0_FK` FOREIGN KEY (`idServiceLabo`) REFERENCES `servicelabo` (`idServiceLabo`),
-  ADD CONSTRAINT `paiement_compteBancaire1_FK` FOREIGN KEY (`idCompte`) REFERENCES `comptebancaire` (`idCompte`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
