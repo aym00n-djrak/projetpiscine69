10a506dad16b433b9658d53f7b9d40e06cddd800
tweasted <78971195+tweasted@users.noreply.github.com>
2022-05-26 12:30:37 +0200

    GraphismeClient

--- CompteClient.php
+++ CompteClient.php
@@ -19,6 +19,7 @@ if ($BDDTrouvee) {
     <link rel="stylesheet" type="text/css" href="Accueil.css">
     <script type="text/javascript">
         $(document).ready(function() {
+
             $('.header').height($(window).height());
         });
     </script>
@@ -45,42 +46,44 @@ if ($BDDTrouvee) {
         </div>
     </nav>
     <form class="formulaireLoginClient" method="POST">
-        <<<<<<< HEAD <div class="container features">
+        <div class="container features">
             <p class="Titre-Section">Connexion</p>
             <div class="row">
                 <div class="col-lg-8 col-md-8 col-sm-12">
                     <table>
                         <tr>
                             <!--Nom-->
-                            <td>Nom</td>
+                            <td>Nom :</td>
                             <td><input type="text" name="nomClient" /></td>
                             <!--Prenom-->
-                            <td>Prenom</td>
+                            <td>Prenom :</td>
                             <td><input type="text" name="prenomClient" /></td>
-                            <!--Code postal-->
-                            <td>Code postal</td>
-                            <td><input type="number" name="codePostalClient" /></td>
                         </tr>
                         <tr>
+                            <!--Code postal-->
+                            <td>Code postal :</td>
+                            <td><input type="number" name="codePostalClient" /></td>
                             <!--Email-->
-                            <td>Email </td>
+                            <td>Email :</td>
                             <td><input type="email" name="courrielClient" /></td>
+                        </tr>
+                        <tr>
                             <!--Carte vitale-->
-                            <td>Carte Vitale </td>
+                            <td>Carte Vitale :</td>
                             <td><input type="number" name="numCarteVitaleClient" /></td>
                             <!--Mot de passe-->
-                            <td>Password </td>
+                            <td>Password :</td>
                             <td><input type="password" name="mdpClient" /></td>
                         </tr>
                         <tr>
                             <!--Numero carte bancaire-->
-                            <td>Ne carte bancaire </td>
+                            <td>Ne carte bancaire :</td>
                             <td><input type="password" name="numCarteBancaireClient" /></td>
                         </tr>
                         <tr>
                             <!--Bouton de validation-->
                             <td colspan="4" align="center">
-                                <input type="submit" name="Connexion" value="Connexion" text-align="center" />
+                                <input type="submit" class="submitform" name="Connexion" value="Connexion" text-align="center" />
                             </td>
                         </tr>
                     </table>
@@ -88,112 +91,37 @@ if ($BDDTrouvee) {
                 <div class="col-lg-4 col-md-4 col-sm-12">
                     <!--Bouton d'inscription-->
                     <h4>Vous n'etes pas membres ?<br>Inscrivez-vous ici :<br></h4>
-                    <a href="http://localhost/projetpiscine69/inscriptionClient.php"><input type="button" value="Inscription"></a>
-                </div>
-
-            </div>
-            </div>
-            <?php
-            if (isset($_POST['Connexion'])) {
-                $nom = isset($_POST['nomClient']) ? $_POST['nomClient'] : " ";
-                $prenom = isset($_POST['prenomClient']) ? $_POST['prenomClient'] : " ";
-                $mdp = isset($_POST['mdpClient']) ? $_POST['mdpClient'] : " ";
-                $codePostal = isset($_POST['codePostalClient']) ? $_POST['codePostalClient'] : " ";
-                $courriel = isset($_POST['courrielClient']) ? $_POST['courrielClient'] : " ";
-                $numCarteVitale = isset($_POST['numCarteVitaleClient']) ? $_POST['numCarteVitaleClient'] : " ";
-                $numCarteBancaire = isset($_POST['numCarteBancaireClient']) ? $_POST['numCarteBancaireClient'] : " ";
-
-                $requete = "SELECT * FROM `client` WHERE (`nomClient` = '$nom' AND `prenomClient` = '$prenom' AND `courrielClient` = '$courriel' AND `motDePasseClient` = '$mdp' AND `codePostalClient` = '$codePostal' AND `carteVitaleClient` = '$numCarteVitale');";
-                $commande = mysqli_query($loginBDD, $requete);
-                $resultat = "on sait pas encore";
-                $donnee = mysqli_fetch_assoc($commande);
-                if ($donnee['idClient'] != null) {
-                    $resultat = "Connexion reussie";
-                    sleep(1);
-                    echo "<script> window.location = 'http://localhost/projetpiscine69/fonctionnalitesClient.html' </script>";
-                } else {
-                    $resultat = "Aucun client trouve";
-                }
-                echo "<br>Resultat identification: $resultat<br><br>";
-            }
-            ?>
-            =======
-            <div class="container features">
-                <p class="Titre-Section">Connexion</p>
-                <div class="row">
-                    <div class="col-lg-8 col-md-8 col-sm-12">
-                        <table>
-                            <tr>
-                                <!--Nom-->
-                                <td>Nom :</td>
-                                <td><input type="text" name="nomClient" /></td>
-                                <!--Prenom-->
-                                <td>Prenom :</td>
-                                <td><input type="text" name="prenomClient" /></td>
-                            </tr>
-                            <tr>
-                                <!--Code postal-->
-                                <td>Code postal :</td>
-                                <td><input type="number" name="codePostalClient" /></td>
-                                <!--Email-->
-                                <td>Email :</td>
-                                <td><input type="email" name="courrielClient" /></td>
-                            </tr>
-                            <tr>
-                                <!--Carte vitale-->
-                                <td>Carte Vitale :</td>
-                                <td><input type="number" name="numCarteVitaleClient" /></td>
-                                <!--Mot de passe-->
-                                <td>Password :</td>
-                                <td><input type="password" name="mdpClient" /></td>
-                            </tr>
-                            <tr>
-                                <!--Numero carte bancaire-->
-                                <td>Ne carte bancaire :</td>
-                                <td><input type="password" name="numCarteBancaireClient" /></td>
-                            </tr>
-                            <tr>
-                                <!--Bouton de validation-->
-                                <td colspan="4" align="center">
-                                    <input type="submit" class="submitform" name="Connexion" value="Connexion" text-align="center" />
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-12">
-                        <!--Bouton d'inscription-->
-                        <h4>Vous n'etes pas membres ?<br>Inscrivez-vous ici :<br></h4>
-                        <a href="CompteClientInscription.html">
-                            <input type="submit" name="Inscription" value="Inscription" text-align="center">
-                        </a>
-                    </div>
+                    <a href="CompteClientInscription.html">
+                        <input type="submit" name="Inscription" value="Inscription" text-align="center">
+                    </a>
                 </div>
             </div>
-            <?php
-            if (isset($_POST['Connexion'])) {
-                $nom = isset($_POST['nomClient']) ? $_POST['nomClient'] : " ";
-                $prenom = isset($_POST['prenomClient']) ? $_POST['prenomClient'] : " ";
-                $mdp = isset($_POST['mdpClient']) ? $_POST['mdpClient'] : " ";
-                $codePostal = isset($_POST['codePostalClient']) ? $_POST['codePostalClient'] : " ";
-                $courriel = isset($_POST['courrielClient']) ? $_POST['courrielClient'] : " ";
-                $numCarteVitale = isset($_POST['numCarteVitaleClient']) ? $_POST['numCarteVitaleClient'] : " ";
-                $numCarteBancaire = isset($_POST['numCarteBancaireClient']) ? $_POST['numCarteBancaireClient'] : " ";
+        </div>
+        <?php
+        if (isset($_POST['Connexion'])) {
+            $nom = isset($_POST['nomClient']) ? $_POST['nomClient'] : " ";
+            $prenom = isset($_POST['prenomClient']) ? $_POST['prenomClient'] : " ";
+            $mdp = isset($_POST['mdpClient']) ? $_POST['mdpClient'] : " ";
+            $codePostal = isset($_POST['codePostalClient']) ? $_POST['codePostalClient'] : " ";
+            $courriel = isset($_POST['courrielClient']) ? $_POST['courrielClient'] : " ";
+            $numCarteVitale = isset($_POST['numCarteVitaleClient']) ? $_POST['numCarteVitaleClient'] : " ";
+            $numCarteBancaire = isset($_POST['numCarteBancaireClient']) ? $_POST['numCarteBancaireClient'] : " ";
 
-                $requete = "SELECT * FROM `client` WHERE (`nomClient` = '$nom' AND `prenomClient` = '$prenom' AND `courrielClient` = '$courriel' AND `motDePasseClient` = '$mdp' AND `codePostalClient` = '$codePostal' AND `carteVitaleClient` = '$numCarteVitale');";
-                $commande = mysqli_query($loginBDD, $requete);
-                $resultat = "on sait pas encore";
-                $donnee = mysqli_fetch_assoc($commande);
-                if ($donnee['idClient'] != null) {
-                    $resultat = "Connexion reussie";
-                    sleep(1);
-                    echo "<script> window.location = 'http://localhost/projetpiscine69/fonctionnalitesClient.html' </script>";
-                } else {
-                    $resultat = "Aucun client trouve";
-                }
-                echo "<br>Resultat identification: $resultat<br><br>";
+            $requete = "SELECT * FROM `client` WHERE (`nomClient` = '$nom' AND `prenomClient` = '$prenom' AND `courrielClient` = '$courriel' AND `motDePasseClient` = '$mdp' AND `codePostalClient` = '$codePostal' AND `carteVitaleClient` = '$numCarteVitale');";
+            $commande = mysqli_query($loginBDD, $requete);
+            $resultat = "on sait pas encore";
+            $donnee = mysqli_fetch_assoc($commande);
+            if ($donnee['idClient'] != null) {
+                $resultat = "Connexion reussie";
+                sleep(1);
+                echo "<script> window.location = 'http://localhost/projetpiscine69/fonctionnalitesClient.html' </script>";
+            } else {
+                $resultat = "Aucun client trouve";
             }
-            ?>
-            >>>>>>> charlesMain
+            echo "<br>Resultat identification: $resultat<br><br>";
+        }
+        ?>
+        >>>>>>> charlesMain
     </form>
 
     <footer class="page-footer">
@@ -201,12 +129,7 @@ if ($BDDTrouvee) {
         <div class="row">
 
             <div class="col-lg-8 col-md-8 col-sm-20">
-
-                <<<<<<< HEAD <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7008.822583384912!2d2.2823564991389347!3d48.851686907281966!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20Paris%20Lyon!5e0!3m2!1sfr!2sfr!4v1653309618172!5m2!1sfr!2sfr" width="100%" height="100%" style="border:0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    =======
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7008.822583384912!2d2.2823564991389347!3d48.851686907281966!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20Paris%20Lyon!5e0!3m2!1sfr!2sfr!4v1653309618172!5m2!1sfr!2sfr" width="100%" height="100%" style="border:0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    >>>>>>> charlesMain
-
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7008.822583384912!2d2.2823564991389347!3d48.851686907281966!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20Paris%20Lyon!5e0!3m2!1sfr!2sfr!4v1653309618172!5m2!1sfr!2sfr" width="100%" height="100%" style="border:0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
             <div class="col-lg-4 col-md-4 col-sm-12">
