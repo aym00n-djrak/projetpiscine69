a8655d8300ad4d4caaee5f0f648a8a0e41113a66
AlbertM96 <57848310+AlbertM96@users.noreply.github.com>
2022-05-28 17:46:52 +0200

    correction recherche medecin

--- phpsearchbar/recherche.php
+++ phpsearchbar/recherche.php
@@ -5,14 +5,14 @@ include "connect.php";
 if (isset($_GET['user'])) {
     $user = (string)  trim($_GET['user']);
 
-    $requeteSQL = "SELECT * FROM `medecin`, `labo` WHERE `prenomMedecin` LIKE '$user%' OR `salleLabo` LIKE '$user%'";
+    $requeteSQL = "SELECT distinct prenommedecin, nommedecin, bureaumedecin, specialitemedecin FROM `medecin`, `labo` WHERE `prenomMedecin` LIKE '$user%' OR `salleLabo` LIKE '$user%'";
     $executequery = mysqli_query($bdd_login, $requeteSQL);
 
     while ($donnee = mysqli_fetch_assoc($executequery)) {
-        if (strpos($donnee['prenomMedecin'],$user) !== false) {
-            echo $donnee['prenomMedecin'] . " " . $donnee['nomMedecin']. " Spécialité: " . $donnee['specialiteMedecin']."<br>";
+        if (strpos($donnee['prenommedecin'],$user) !== false) {
+            echo $donnee['prenommedecin'] . " " . $donnee['nommedecin']. " <b>Spécialité:</b> " . $donnee['specialitemedecin']."<br>";
         } else if(strpos($donnee['salleLabo'],$user) !== false) {
-            echo " " . $donnee['salleLabo']. " Téléphone: ".$donnee['telLabo']."<br>";
+            echo " " . $donnee['salleLabo']. " <b>Téléphone:</b> ".$donnee['telLabo']."<br>";
         }
     }
 }
